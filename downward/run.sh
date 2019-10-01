79cd0a61e9ca2e0cdeba22b1673019f2f3683ae0
kaizerw <kaizerwesley@gmail.com>
2019-10-01 16:28:39 -0300

    added astar_is_better field

--- downward/run.sh
+++ downward/run.sh
@@ -45,17 +45,23 @@ run_pref="./fast-downward.py --overall-memory-limit 3584M --overall-time-limit 1
 #opts="socwsss_cplex(constraint_type=3, constraint_generators=_, heuristic=lmcut, hstar=true, mip_start=false)"
 
 
-#opts="socwsss_cplex(constraint_type=3, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false)"
+#opts="socwsss_cplex(constraint_type=3, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false, callbacks=lazy_heuristic)"
 #$run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
 
-#opts="socwsss_cplex(sat_seq=true, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false)"
+#opts="socwsss_cplex(sat_seq=true, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false, callbacks=lazy_heuristic)"
 #$run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
 
-#opts="socwsss_cplex(best_seq=true, constraint_type=3, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false)"
+#opts="socwsss_cplex(best_seq=true, constraint_type=3, constraint_generators=_, heuristic=lmcut, hstar_search=true, mip_start=false, callbacks=lazy_heuristic)"
 #$run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
 
 
-opts="socwsss_cplex(constraint_type=3, constraint_generators=seq_landmarks, hstar_pdb=true)"
+#opts="socwsss_cplex(constraint_type=3, constraint_generators=seq_landmarks, hstar_pdb=true, callbacks=lazy_heuristic)"
+#$run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
+
+#opts="socwsss_cplex(sat_seq=true, constraint_generators=seq_landmarks, hstar_pdb=true, callbacks=lazy_heuristic)"
+#$run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
+
+opts="socwsss_cplex(best_seq=true, constraint_type=3, constraint_generators=seq_landmarks, hstar_pdb=true, callbacks=lazy_heuristic)"
 $run_pref $DOWNWARD_BENCHMARKS/visitall-opt11-strips/problem03-full.sas --search "$opts"
 
 
